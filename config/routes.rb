ece6c04c408af2bb001e0fe83ef7ad5d586d7ebb
naotoakiyama <na70101121@icloud.com>
2018-02-25 12:40:04 +0900

    Preparing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resouces :users
-  resouces :groups
-  resouces :members
-  resouces :messages
+  resources :users
+  resources :groups
+  resources :members
+  resources :messages
   root 'messages#index'
 end
